d8110da70df6b07f85958b974020e2bea7da8c71
Victor Neo <icyisamu@gmail.com>
2012-09-15 20:52:15 +0800

    Search listings and collections done

--- uiconnect/settings.py
+++ uiconnect/settings.py
@@ -132,6 +132,7 @@ INSTALLED_APPS = (
     'accounts',
     'listings',
     'categories',
+    'search',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
