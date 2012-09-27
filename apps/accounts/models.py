c00dae18c8de7f4c1f1ef83f90088bf180f9d68c
Victor Neo <icyisamu@gmail.com>
2012-09-28 01:39:59 +0800

    Relationships and some funny stylings

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -10,13 +10,35 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name='profile')
     avatar = ProcessedImageField([ResizeToFill(100, 100),], upload_to='avatars', format='PNG', null=True, blank=True, default=None)
     bio = models.TextField(max_length=500, null=True, blank=True)
-    following = models.ManyToManyField('self', related_name='followers', symmetrical=False)
+    following = models.ManyToManyField('self', through='Relationship', related_name='followers', symmetrical=False)
     fb_id = models.CharField(max_length=200, null=True, blank=True)
 
     points = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.user.first_name
+
+    def add_relationship(self, profile):
+        relationship, created = Relationship.objects.get_or_create(
+            from_user=self,
+            to_user=profile,
+        )
+        return relationship
+
+    def remove_relationship(self, profile):
+        Relationship.objects.filter(
+            from_user=self,
+            to_user=profile,
+        ).delete()
+        return
+
+    def get_followers(self):
+        return self.followers.filter(from_user__to_user=self).all()
+
+
+class Relationship(models.Model):
+    from_user = models.ForeignKey(UserProfile, related_name='from_user')
+    to_user = models.ForeignKey(UserProfile, related_name='to_user')
 
 
 WELCOME_MSG = '''
