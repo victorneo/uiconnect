2c67c6641322011f13735a2e05ea5689637488bc
Victor Neo <icyisamu@gmail.com>
2012-10-02 02:19:02 +0800

    Added profile links to profile page

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 from django.contrib import messages
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 import facebook
 import requests
 from uiconnect import settings
